e2aec98179c5eda083022f948bdc20370dfd3492
Andrea SCIABA <Andrea.Sciaba@cern.ch>
2019-09-24 11:43:07 +0200

    Creating SW directory

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,9 +4,14 @@
 export LANG=C
 export VO_CMS_SW_DIR=/data/patatrack
 export SCRAM_ARCH=slc7_amd64_gcc820
+mkdir -p $VO_CMS_SW_DIR
 wget http://cmsrep.cern.ch/cmssw/bootstrap.sh -O $VO_CMS_SW_DIR/bootstrap.sh
 chmod a+x $VO_CMS_SW_DIR/bootstrap.sh
 $VO_CMS_SW_DIR/bootstrap.sh -a $SCRAM_ARCH -r cms -path $VO_CMS_SW_DIR setup
+if [[ $? -ne 0 ]] ; then
+    echo "[ERROR] Could not bootstrap installation"
+    exit 1
+fi
 
 # CMSSW installation
 $VO_CMS_SW_DIR/common/cmspkg -a $SCRAM_ARCH upgrade -y
